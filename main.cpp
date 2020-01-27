38a64953b5fe1e080495f210325cb692641a9cf3
giulianobernardi <g.bernardi.mnos@gmail.com>
2020-01-27 14:41:47 +0000

    Added 10 nouns and corresponding 3 actions each

--- main.cpp
+++ main.cpp
@@ -33,88 +33,116 @@ arm.rotateCounterClockwise(); //demonstrates CamelCase
 
 /*
  1)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: hand
+ action 1: the hand grabs
+ action 2: the hand makes an OK sign
+ action 3: the hand crosses the index and the middle finger
  */
+ hand.grab();
+ hand.makeOKSign();
+ hand.crossFingers(index, middle);
 
 
 /*
  2)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: car
+ action 1: the car starts
+ action 2: the car stops
+ action 3: the car turns on the front lights
  */
+ car.start();
+ car.stop();
+ car.turnFrontLightsON();
 
 /*
  3)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: person
+ action 1: the person walks
+ action 2: the person says the word "hello"
+ action 3: the person jumps
  */
+ person.walks();
+ person.say("hello");
+ person.jump();
 
 
 /*
  4)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: Box
+ action 1: the box opens itself
+ action 2: tge box closes itself
+ action 3: the box locks itself
  */
+ box.open();
+ box.close();
+ box.lock();
 
 /*
  5)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: cat 
+ action 1: the meows
+ action 2: the cat purrs
+ action 3: the cat sits
  */
-
+cat.meow();
+cat.purr();
+cat.sit();
 
 /*
  6)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: pen
+ action 1: the pen's cap is removed 
+ action 2: the pen writes the word "hello"
+ action 3: the pen is recharged
  */
+ pen.removeCap();
+ pen.write("hello");
+ pen.addInk();
 
 /*
  7)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: plane
+ action 1: the plane takes off
+ action 2: the plane lands
+ action 3: the plane is refueled with 1000 l of fuel
  */
-
+plane.takeOff();
+plane.land();
+plane.refuel(1000);
 
 /*
- 8)
- Noun:
- action 1:
- action 2:
- action 3:
+ 8) 
+ Noun: computer
+ action 1: the computer boots up
+ action 2: the computer launchs the task manager
+ action 3: the computer scans itself for viruses
  */
+ computer.bootUp();
+ computer.launchTaskManager();
+ computer.scanForViruses();
 
 /*
  9)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: Audio Plugin
+ action 1: audio plugin plays sound
+ action 2: audio plugin increase the pitch
+ action 3: audio plugin add reverb
  */
+ audioPlugin.play(sound);
+ audioPlugin.pitchIncrease();
+ audioPlugin.addReverb();
 
 
 /*
  10)
- Noun:
- action 1:
- action 2:
- action 3:
+ Noun: email
+ action 1: send email
+ action 2: read email 
+ action 3: correct the grammar mistakes in the email
  */
+ email.send();
+ email.read();
+ email.correctGrammar();
 
 
 #endif
